44509a2c08993749cb612b9b93122ab858e7da75
sasi5868 <sasidharanaa@gmail.com>
2022-03-21 11:52:58 +0530

    testing

--- server/controllers/audio.js
+++ server/controllers/audio.js
@@ -3,23 +3,11 @@ const fs = require('fs');
 function readFile(nlpResponseFilePath) {
   let nlpResponse = new Promise(async (resolve, reject) => {
     console.log("nlpResponseFilePath inside ReadFile", nlpResponseFilePath);
-    // setTimeout(async () => {
-      // await watchFile(nlpResponseFilePath).then((watchResponse) => {
-      //   console.log("watchResponse in readFile", watchResponse);
-        // if (watchResponse != null) {
           setTimeout(async () => {
             let readFileResponse = await fs.readFileSync(nlpResponseFilePath);
             // console.log("nlpResponse in readFile", readFileResponse);
             resolve(readFileResponse);
           }, 100);
-        // } else {
-        //   resolve(null);
-        // }
-    //   }).catch((err) => {
-    //     console.log("error while watching and reading file sending null response", err);
-    //     resolve(null);
-    //   })
-    // }, 500);
   });
   return nlpResponse;
 }
@@ -86,54 +74,26 @@ function executeScriptNlp(nlpExecutionFile) {
 exports.postAudio = async (req, res, next) => {
   console.log("saved audio file using multer", req.body.name);
   nlpResponse = {};
-  languageId = req.body.languageId;
-  console.log("languageId: ", languageId);
 
   asrInputFile = "/home/istl/demo/voice_channel_navigation/backend/data/audio/sasi@gmail.com/" + req.body.name;
   console.log("asrInputFile: ",asrInputFile);
-  if(languageId == 0) {
-    asrResponseFileName = req.body.name.replace("wav", "txt");
-    asrResponseFilePath = "/home/istl/build/bbnl_english_nlp/process_dir/" + asrResponseFileName;
-    asrExecutionFile = "/home/istl/build/kaldi/egs/bbnl_english_asr/s5/online_run.sh " + asrInputFile + " " + asrResponseFilePath;
+  asrResponseFileName = req.body.name.replace("wav", "txt");
+  asrResponseFilePath = "/home/istl/build/bbnl_english_nlp/process_dir/" + asrResponseFileName;
+  asrExecutionFile = "/home/istl/build/kaldi/egs/bbnl_english_asr/s5/online_run.sh " + asrInputFile + " " + asrResponseFilePath;
 
-    nlpResponseFileName = req.body.name.replace("wav", "json");
-    nlpResponseFilePath = "/home/istl/build/bbnl_english_nlp/out_dir/" + nlpResponseFileName;
-    nlpExecutionFile = "python3.8 /home/istl/build/bbnl_english_nlp/01_process_intent_english.py " + asrResponseFilePath + " " + nlpResponseFilePath;
-  } else if(languageId == 1) {
-    asrResponseFileName = req.body.name.replace("wav", "txt");
-    asrResponseFilePath = "/home/istl/build/bbnl_hindi_nlp/process_dir/" + asrResponseFileName;
-    asrExecutionFile = "/home/istl/build/kaldi/egs/bbnl_hindi_asr/s5/online_run.sh " + asrInputFile + " " + asrResponseFilePath;
+  nlpResponseFileName = req.body.name.replace("wav", "json");
+  nlpResponseFilePath = "/home/istl/build/bbnl_english_nlp/out_dir/" + nlpResponseFileName;
+  nlpExecutionFile = "python3.8 /home/istl/build/bbnl_english_nlp/01_process_intent_english.py " + asrResponseFilePath + " " + nlpResponseFilePath;
 
-    nlpResponseFileName = req.body.name.replace("wav", "json");
-    nlpResponseFilePath = "/home/istl/build/bbnl_hindi_nlp/out_dir/" + nlpResponseFileName;
-    nlpExecutionFile = "python3.8 /home/istl/build/bbnl_hindi_nlp/01_process_intent_hindi.py " + asrResponseFilePath + " " + nlpResponseFilePath;
-  } else if(languageId == 2) {
-    asrResponseFileName = req.body.name.replace("wav", "txt");
-    asrResponseFilePath = "/home/istl/build/bbnl_tamil_nlp/process_dir/" + asrResponseFileName;
-    asrExecutionFile = "/home/istl/build/kaldi/egs/bbnl_tamil_asr/s5/online_run.sh " + asrInputFile + " " + asrResponseFilePath;
+  nlpResponse = {"gif_car_model": "Ford_Figo","tsp_files": ["001.tsp","002.tsp","003.tsp"], "excel_file": "YES"}; // For reference
 
-    nlpResponseFileName = req.body.name.replace("wav", "json");
-    nlpResponseFilePath = "/home/istl/build/bbnl_tamil_nlp/out_dir/" + nlpResponseFileName;
-    nlpExecutionFile = "python3.8 /home/istl/build/bbnl_tamil_nlp/01_process_intent_tamil.py " + asrResponseFilePath + " " + nlpResponseFilePath;
-  } else if(languageId == 3) {
-    asrResponseFileName = req.body.name.replace("wav", "txt");
-    asrResponseFilePath = "/home/istl/build/bbnl_kannada_nlp/process_dir/" + asrResponseFileName;
-    asrExecutionFile = "/home/istl/build/kaldi/egs/bbnl_kannada_asr/s5/online_run.sh " + asrInputFile + " " + asrResponseFilePath;
-
-    nlpResponseFileName = req.body.name.replace("wav", "json");
-    nlpResponseFilePath = "/home/istl/build/bbnl_kannada_nlp/out_dir/" + nlpResponseFileName;
-    nlpExecutionFile = "python3.8 /home/istl/build/bbnl_kannada_nlp/01_process_intent_kannada.py " + asrResponseFilePath + " " + nlpResponseFilePath;
-  } else {
-    console.log("Demo is not ready for this language");
-    res.status(200).json({
-      ok: false,
-      message: "Demo is not ready for this language",
-      intent: "NO_FILE",
-      nlpResponse: {}
-    });
-    return;
-  }
-  // nlpResponse = {"channel": "2", "volume": "2", "subscription": "YES", "program": "YES", "quality": "HD"}; // For reference
+  console.log("nlpResponse", nlpResponse);
+  res.status(200).json({
+    ok: true,
+    message: "NLP response received",
+    nlpResponse: nlpResponse
+  });
+  return;
 
   executeScriptAsr(asrExecutionFile).then((response) => {
     console.log("ASR script execution response:", response);
